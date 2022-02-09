839e5c19ef7cc88b73176cb91d112faa4793b49c
Akindele-Ebenezer <83076721+Akindele-Ebenezer@users.noreply.github.com>
2022-02-09 11:25:59 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,155 @@
+<?php
+    include "header.php";
+?>
+
+<main>
+    <div class="body">   
+
+        
+        <section class="sec-1">
+
+            <div class="sec-1-inner">
+                <div>
+                    <h1><?= date('l d, 20y') ?></h1>
+                    <h2>Company Name</h2>
+                    <p> Our nature of business include Vehicles Alignment, Car Service,   Installation of Inverter and Solar System, Resale of Refurbished Company Used Vehicle, Iron Gates and Doors, Paints and even more.</p>
+                    <button>CONTACT US</button>
+                </div>
+            </div>
+    
+            <div>
+                <img src="images/bg-1.jpeg" alt="">
+                <img src="images/bg-2.jpeg" alt="">
+                <img src="images/bg-3.jpg" alt="">
+                <img src="images/bg-4.jpeg" alt=""> 
+            </div>
+
+        </section>    
+        
+        
+        <div class="sec-2-title">
+            <h1>SERVICES</h1>
+        </div>
+        
+        <section class="sec-2">
+            
+            <div class="box">
+                <div style="background-image: url('images/services-1.jpeg')"></div>
+                <h1>Car Service</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque dolores autem delectus asperiores quae voluptatibus nemo unde debitis beatae necessitatibus quibusdam placeat laboriosam, temporibus nesciunt voluptas minima nihil quia tenetur!5</p>
+                <button>NOTIFY</button>
+            </div>
+
+            <div class="box">
+                <div style="background-image: url('images/services-2.jpg')"></div>
+                <h1>Vehicle Alignment</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque dolores autem delectus asperiores quae voluptatibus nemo unde debitis beatae necessitatibus quibusdam placeat laboriosam, temporibus nesciunt voluptas minima nihil quia tenetur!5</p>
+                <button>NOTIFY</button>
+            </div>
+
+            <div class="box">
+                <div style="background-image: url('images/services-3.jpg')"></div>
+                <h1>Installation of Inverter and Solar System</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque dolores autem delectus asperiores quae voluptatibus nemo unde debitis beatae necessitatibus quibusdam placeat laboriosam, temporibus nesciunt voluptas minima nihil quia tenetur!5</p>
+                <button>NOTIFY</button>
+            </div>
+
+            <div class="box">
+                <div style="background-image: url('images/services-4.png')"></div>
+                <h1>Resale of Refurbished Company used Vehicle</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque dolores autem delectus asperiores quae voluptatibus nemo unde debitis beatae necessitatibus quibusdam placeat laboriosam, temporibus nesciunt voluptas minima nihil quia tenetur!5</p>
+                <button>NOTIFY</button>
+            </div>
+
+        </section>
+
+        <section class="sec-3">
+
+            <!-- IMAGE -->
+            <div>
+                <h1>Need Help?</h1>
+                <p>Call us now for urgent jobs. <br> We render services very accurately, as fast as we can.</p>
+                <button>CALL</button>
+            </div>
+
+        </section>
+
+        <section class="sec-4">
+
+            <div>
+                <h1>SUPPLIES</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint autem labore itaque voluptatibus dolore cum repellendus officia quae, optio debitis, fuga voluptas exercitationem dignissimos veritatis qui eaque nostrum quidem dolorem?
+                Deserunt soluta fugit cum! Mollitia, doloremque blanditiis similique harum cupiditate molestias illum ipsam natus libero, perferendis itaque. Rerum soluta eos consectetur! Maxime molestias nostrum repellendus dicta sint perspiciatis quia incidunt. </p>
+            </div>
+
+        </section>
+
+        <section class="sec-5">
+
+        <div class="box">
+                <div style="background-image: url('images/services-4.png')"></div>
+                <h1>Doors</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque dolores autem delectus asperiores quae voluptatibus nemo unde debitis beatae necessitatibus quibusdam placeat laboriosam, temporibus nesciunt voluptas minima nihil quia tenetur!5</p>
+                <button>NOTIFY</button>
+            </div>
+            
+            <div class="box">
+                <div style="background-image: url('images/services-4.png')"></div>
+                <h1>Iron Gates</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque dolores autem delectus asperiores quae voluptatibus nemo unde debitis beatae necessitatibus quibusdam placeat laboriosam, temporibus nesciunt voluptas minima nihil quia tenetur!5</p>
+                <button>NOTIFY</button>
+            </div>
+            <div class="box">
+                <div style="background-image: url('images/img-1.jpeg')"></div>
+                <h1>Paints</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores illum deserunt labore dolorum exercitationem velit pariatur doloribus odio dolorem doloremque natus fugiat, voluptate est repudiandae corporis tempore. Aperiam, vero magni.
+                Illo quos eligendi quam eos ratione. </p>
+                <button>NOTIFY</button>
+            </div>
+
+        </section>
+
+        <section><!-- image --></section>
+
+        <section class="sec-6">
+
+            <div class="sec-inner-wrapper">
+                <div class="sec-inner">
+                    <div>
+                        <h1>Areas We Cover In NIGERIA</h1>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat id quo autem molestias vitae, rerum fuga optio reiciendis velit illo enim repudiandae cupiditate maxime asperiores iusto dolores? Vitae, earum nesciunt.</p>
+                    </div>
+
+                    <div>
+                        <h1>About Us</h1>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat id quo autem molestias vitae, rerum fuga optio reiciendis velit illo enim repudiandae cupiditate maxime asperiores iusto dolores? Vitae, earum nesciunt.</p>
+                        <button>Read More</button>
+                    </div>
+                </div>
+                <div class="img">
+                    <img src="images/bg-1.jpeg" alt="">
+                    <img src="images/bg-2.jpeg" alt="">
+                    <img src="images/bg-3.jpg" alt="">
+                    <img src="images/bg-4.jpeg" alt=""> 
+                </div>
+            </div>
+
+        </section>
+
+        <section class="sec-1">
+            
+            <div class="sec-1-inner">
+                <div>
+                    <h1>Individual Consultation</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque eius quas architecto sapiente. Repellat quos a odio, ipsam repellendus sapiente accusantium et iusto voluptates recusandae, reprehenderit natus asperiores! Ratione, cumque.</p>
+                    <button>CONTACT US</button>
+                </div>
+            </div>
+            
+            <div style="background-image: url('images/img-1.jpeg');"></div>     
+
+        </section> 
+        
+        <?php
+            include "footer.php";
+        ?>
