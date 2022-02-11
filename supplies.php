c052732df633dd934e7b40ac839d033a0338b03b
Akindele-Ebenezer <83076721+Akindele-Ebenezer@users.noreply.github.com>
2022-02-11 11:00:58 +0100

    Add files via upload

---
+++ supplies.php
@@ -0,0 +1,31 @@
+<?php 
+    $title = "What We Supply";
+    $content = " 
+                <section class='sec-5'> 
+
+                    <div class='box'>
+                        <div style=\"background-image: url('images/doors.jpg'); background-size: contain;\"></div>
+                        <h1>Doors</h1>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque dolores autem delectus asperiores quae voluptatibus nemo unde debitis beatae necessitatibus quibusdam placeat laboriosam, temporibus nesciunt voluptas minima nihil quia tenetur!5</p>
+                        <button>NOTIFY</button>
+                    </div>
+                    
+                    <div class='box'>
+                        <div style=\"background-image: url('images/iron-gates.jpg')\"></div>
+                        <h1>Iron Gates</h1>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque dolores autem delectus asperiores quae voluptatibus nemo unde debitis beatae necessitatibus quibusdam placeat laboriosam, temporibus nesciunt voluptas minima nihil quia tenetur!5</p>
+                        <button>NOTIFY</button>
+                    </div>
+                    <div class='box'>
+                        <div style=\"background-image: url('images/paints.jpg')\"></div>
+                        <h1>Paints</h1>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores illum deserunt labore dolorum exercitationem velit pariatur doloribus odio dolorem doloremque natus fugiat, voluptate est repudiandae corporis tempore. Aperiam, vero magni.
+                        Illo quos eligendi quam eos ratione. </p>
+                        <button>NOTIFY</button>
+                    </div>
+
+                </section>";
+                include "temp.php";
+?>
+
+ 
---
+++ temp.php
@@ -0,0 +1,14 @@
+<?php 
+    include "header.php"; 
+?>
+
+    <div class="title">
+        <h1><?= $title; ?></h1>
+    </div>
+
+
+    <?= $content; ?>
+
+<?php 
+    include "footer.php";
+?>
