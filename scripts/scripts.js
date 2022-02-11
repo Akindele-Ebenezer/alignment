1e8f8961959ac464d241354d4af62288c61abc21
Akindele-Ebenezer <83076721+Akindele-Ebenezer@users.noreply.github.com>
2022-02-11 12:33:55 +0100

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,5 +2,5 @@ const viewAllSuppliesButton = document.querySelector(".sec-4 div button");
 
 viewAllSuppliesButton.addEventListener("click", () => {
     window.open("https://ebengm.herokuapp.com/supplies.php")
-})
+});
 //console.log(allSupplies);
