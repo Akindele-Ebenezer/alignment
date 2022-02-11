a7ca4fa9dccf9465c9b9c49369d955802f36d269
Akindele-Ebenezer <83076721+Akindele-Ebenezer@users.noreply.github.com>
2022-02-11 11:47:17 +0100

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,6 +1,6 @@
 const allSupplies = document.querySelector(".sec-4 div button");
 
 allSupplies.addEventListener("click", () => {
-    window.open("http://127.0.0.1/car-alignment/supplies.php")
+    window.open("https://ebengm.herokuapp.com/supplies.php")
 })
 console.log(allSupplies);
